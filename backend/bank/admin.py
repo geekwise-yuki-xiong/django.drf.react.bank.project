d60a73893ad2445521ee7d1b58dd56e3ed79a9ea
geekwise-yuki-xiong <geekwise.yuki.xiong@gmail.com>
2019-12-16 11:08:10 -0800

    update: added inline app register for django bank models

--- backend/bank/admin.py
+++ backend/bank/admin.py
@@ -4,8 +4,20 @@ from bank.models import Branch, Customer, Account, Product
 # Register your models here.
 
 admin.site.register((
-    Branch,
-    Customer,
     Account,
     Product,
 ))
+
+class ProductInline(admin.StackedInline):
+    model = Product
+
+class AccountInline(admin.StackedInline):
+    model = Account
+
+@admin.register(Branch)
+class BranchAdmin(admin.ModelAdmin):
+    inlines = [ProductInline, AccountInline]
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    inlines = [AccountInline]
