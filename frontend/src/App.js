530d4ba35295b666d5ac3cc6e815aee734f56ec7
geekwise-yuki-xiong <geekwise.yuki.xiong@gmail.com>
2019-12-17 11:22:54 -0800

    update: changed static html to include bank analogy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,158 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+ // frontend/src/App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ import React, { Component } from "react";
+ import Modal from "./components/Modal";
+ import axios from "axios";
 
-export default App;
+ class App extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       viewCompleted: false,
+       activeItem: {
+         title: "",
+         description: "",
+         completed: false
+       },
+       todoList: []
+     };
+   }
+   componentDidMount() {
+     this.refreshList();
+   }
+   refreshList = () => {
+     axios
+       .get("https://django-react-todo-yuki.herokuapp.com/api/todos/")
+       .then(res => this.setState({ todoList: res.data.results }))
+       .catch(err => console.log(err));
+   };
+   displayCompleted = status => {
+     if (status) {
+       return this.setState({ viewCompleted: true });
+     }
+     return this.setState({ viewCompleted: false });
+   };
+   renderTabList = () => {
+     return (
+       <div className="my-5 tab-list">
+         <span
+           onClick={() => this.displayCompleted(true)}
+           className={this.state.viewCompleted ? "active" : ""}
+         >
+           Branches
+         </span>
+         <span
+           onClick={() => this.displayCompleted(false)}
+           className={this.state.viewCompleted ? "" : "active"}
+         >
+           Customers
+         </span>
+         <span
+           onClick={() => this.displayCompleted(false)}
+           className={this.state.viewCompleted ? "" : "active"}
+         >
+           Products
+         </span>
+         <span
+           onClick={() => this.displayCompleted(false)}
+           className={this.state.viewCompleted ? "" : "active"}
+         >
+           Accounts
+         </span>
+       </div>
+     );
+   };
+   renderItems = () => {
+     const { viewCompleted } = this.state;
+     const newItems = this.state.todoList.filter(
+       item => item.completed === viewCompleted
+     );
+     return newItems.map(item => (
+       <li
+         key={item.id}
+         className="list-group-item d-flex justify-content-between align-items-center"
+       >
+         <span
+           className={`todo-title mr-2 ${
+             this.state.viewCompleted ? "completed-todo" : ""
+           }`}
+           title={item.description}
+         >
+           {item.title}
+         </span>
+         <span>
+           <button
+             onClick={() => this.editItem(item)}
+             className="btn btn-secondary mr-2"
+           >
+             {" "}
+             Edit{" "}
+           </button>
+           <button
+             onClick={() => this.handleDelete(item)}
+             className="btn btn-danger"
+           >
+             Delete{" "}
+           </button>
+         </span>
+       </li>
+     ));
+   };
+   toggle = () => {
+     this.setState({ modal: !this.state.modal });
+   };
+   handleSubmit = item => {
+     this.toggle();
+     if (item.id) {
+       axios
+         .put(`https://django-react-todo-yuki.herokuapp.com/api/todos/${item.id}/`, item)
+         .then(res => this.refreshList());
+       return;
+     }
+     axios
+       .post("https://django-react-todo-yuki.herokuapp.com/api/todos/", item)
+       .then(res => this.refreshList());
+   };
+   handleDelete = item => {
+     axios
+       .delete(`https://django-react-todo-yuki.herokuapp.com/api/todos/${item.id}`)
+       .then(res => this.refreshList());
+   };
+   createItem = () => {
+     const item = { title: "", description: "", completed: false };
+     this.setState({ activeItem: item, modal: !this.state.modal });
+   };
+   editItem = item => {
+     this.setState({ activeItem: item, modal: !this.state.modal });
+   };
+   render() {
+     return (
+       <main className="content">
+         <h1 className="text-white text-uppercase text-center my-4">Bank App</h1>
+         <div className="row ">
+           <div className="col-md-6 col-sm-10 mx-auto p-0">
+             <div className="card p-3">
+               <div className="">
+                 <button onClick={this.createItem} className="btn btn-primary">
+                   Add item
+                 </button>
+               </div>
+               {this.renderTabList()}
+               <ul className="list-group list-group-flush">
+                 {this.renderItems()}
+               </ul>
+             </div>
+           </div>
+         </div>
+         {this.state.modal ? (
+           <Modal
+             activeItem={this.state.activeItem}
+             toggle={this.toggle}
+             onSave={this.handleSubmit}
+           />
+         ) : null}
+       </main>
+     );
+   }
+ }
+ export default App;
