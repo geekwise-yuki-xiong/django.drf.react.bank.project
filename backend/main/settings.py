d10cec1f34ddfa207be7d6492d5317bb67905ea9
geekwise-yuki-xiong <geekwise.yuki.xiong@gmail.com>
2019-12-17 14:46:50 -0800

    update: added corsheaders

--- backend/main/settings.py
+++ backend/main/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
     'bank',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -127,3 +129,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 django_heroku.settings(locals())
+
+CORS_ORIGIN_WHITELIST = (
+        'http://localhost:3000',
+    )
