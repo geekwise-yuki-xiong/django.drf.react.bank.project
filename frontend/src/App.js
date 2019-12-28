9f9922f642a91c96ed9af06ac148f7294bbe14e1
geekwise-yuki-xiong <geekwise.yuki.xiong@gmail.com>
2019-12-27 18:55:29 -0800

    update: setup working local frontend for master

---
+++ frontend/src/App.js
@@ -0,0 +1,159 @@
+// frontend/src/App.js
+
+import React, { Component } from "react";
+import Modal from "./components/branchModal";
+import axios from "axios";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: true,
+      activeItem: {
+        bank_name: "",
+        location: "",
+      },
+      branchList: []
+    };
+  }
+  componentDidMount() {
+    this.refreshList();
+    console.log("Hello SSH key is working for everyone!")
+  }
+  refreshList = () => {
+    axios
+      .get("https://backend-bank-yuki.herokuapp.com/bank/branches/")
+      .then(res => this.setState({ branchList: res.data.results }))
+      .catch(err => console.log(err));
+  };
+  displayCompleted = status => {
+    if (status) {
+      return this.setState({ viewCompleted: true });
+    }
+    return this.setState({ viewCompleted: false });
+  };
+  renderTabList = () => {
+    return (
+      <div className="my-5 tab-list">
+        <span
+          onClick={() => this.displayCompleted(true)}
+          className={this.state.viewCompleted ? "active" : ""}
+        >
+          Branches
+        </span>
+        {/* <span
+          onClick={() => this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "" : "active"}
+        >
+          Customers
+        </span>
+        <span
+          onClick={() => this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "" : "active"}
+        >
+          Products
+        </span>
+        <span
+          onClick={() => this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "" : "active"}
+        >
+          Accounts
+        </span> */}
+      </div>
+    );
+  };
+  renderItems = () => {
+  //  const { viewCompleted } = this.state;
+  //  const newItems = this.state.todoList.filter(
+  //    item => item.completed === viewCompleted
+  //  );
+  const newItems = this.state.branchList
+    return newItems.map(item => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={`todo-title mr-2 ${
+            this.state.viewCompleted ? "completed-todo" : ""
+          }`}
+          title={item.bank_name}
+        >
+          {item.bank_name}
+        </span>
+        <span>
+          <button
+            onClick={() => this.editItem(item)}
+            className="btn btn-secondary mr-2"
+          >
+            {" "}
+            Edit{" "}
+          </button>
+          <button
+            onClick={() => this.handleDelete(item)}
+            className="btn btn-danger"
+          >
+            Delete{" "}
+          </button>
+        </span>
+      </li>
+    ));
+  };
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+  handleSubmit = item => {
+    this.toggle();
+    if (item.id) {
+      axios
+        .put(`https://backend-bank-yuki.herokuapp.com/bank/branches/${item.id}/`, item)
+        .then(res => this.refreshList());
+      return;
+    }
+    axios
+      .post("https://backend-bank-yuki.herokuapp.com/bank/branches/", item)
+      .then(res => this.refreshList());
+  };
+  handleDelete = item => {
+    axios
+      .delete(`https://backend-bank-yuki.herokuapp.com/bank/branches/${item.id}`)
+      .then(res => this.refreshList());
+  };
+  createItem = () => {
+    const item = { bank_name: "", location: ""};
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+  editItem = item => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+  render() {
+    return (
+      <main className="content">
+        <h1 className="text-white text-uppercase text-center my-4">Bank App</h1>
+        <div className="row ">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="">
+                <button onClick={this.createItem} className="btn btn-primary">
+                  Add item
+                </button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+        {this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={this.handleSubmit}
+          />
+        ) : null}
+      </main>
+    );
+  }
+}
+export default App;
---
+++ frontend/src/components/branchModal.js
@@ -0,0 +1,68 @@
+// frontend/src/components/Modal.js
+
+import React, { Component } from "react";
+import {
+Button,
+Modal,
+ModalHeader,
+ModalBody,
+ModalFooter,
+Form,
+FormGroup,
+Input,
+Label
+} from "reactstrap";
+
+export default class CustomModal extends Component {
+constructor(props) {
+    super(props);
+    this.state = {
+    activeItem: this.props.activeItem
+    };
+}
+handleChange = e => {
+    let { name, value } = e.target;
+    if (e.target.type === "checkbox") {
+    value = e.target.checked;
+    }
+    const activeItem = { ...this.state.activeItem, [name]: value };
+    this.setState({ activeItem });
+};
+render() {
+    const { toggle, onSave } = this.props;
+    return (
+    <Modal isOpen={true} toggle={toggle}>
+        <ModalHeader toggle={toggle}> Branch Item </ModalHeader>
+        <ModalBody>
+        <Form>
+            <FormGroup>
+            <Label for="bank_name">Bank Name</Label>
+            <Input
+                type="text"
+                name="bank_name"
+                value={this.state.activeItem.bank_name}
+                onChange={this.handleChange}
+                placeholder="Enter the bank's name"
+            />
+            </FormGroup>
+            <FormGroup>
+            <Label for="location">Location</Label>
+            <Input
+                type="text"
+                name="location"
+                value={this.state.activeItem.location}
+                onChange={this.handleChange}
+                placeholder="Enter the bank's location"
+            />
+            </FormGroup>
+        </Form>
+        </ModalBody>
+        <ModalFooter>
+        <Button color="success" onClick={() => onSave(this.state.activeItem)}>
+            Save
+        </Button>
+        </ModalFooter>
+    </Modal>
+    );
+}
+}
