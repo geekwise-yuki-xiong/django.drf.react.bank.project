53367cf09ec56a76cffff954000751a37bae3a11
geekwise-yuki-xiong <geekwise.yuki.xiong@gmail.com>
2020-01-23 14:20:43 -0800

    update: merged Maria's homepage

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -2,11 +2,34 @@ import React, { Component } from 'react';
 
 export class Home extends Component {
 
-    render() {
-       return(
-           <h1 >Home page</h1>
-       )
+    divStyle={
+        // background: 'white',
+        padding: '30px',
+        color: 'white'
     }
+
+    tbdbox={
+        background: 'white',
+        width: '530px',
+        hight: '100px',
+        borderRadius: '10%'
+    }
+    
+    render() {
+    return (
+        <div className="row-container" style={this.divStyle}>
+        <div className="col-md-12">
+        <div className="dark-background">
+            <h1 className="headline-1 text-center mx-auto">TBD</h1>
+            <h2 className="text-center">Still waiting...</h2>
+            <div className="mx-auto" style={this.tbdbox}>
+                <img src={require('./tbd(2).png')} />
+            </div>
+        </div>
+        </div>
+        </div>
+    )  
+    };
 }
 
 export default Home;
